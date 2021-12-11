30e8b6badfbe0d5592a9d871502f3742878b5c51
mohanreddy7892 <74665117+mohanreddy7892@users.noreply.github.com>
2021-12-11 19:34:51 +0530

    Update File_process.py

--- File_process.py
+++ File_process.py
@@ -1,5 +1,6 @@
+#this script convert csv file to pipe seperated csv file 
 from contextlib import nullcontext
-from numpy import str_
+import numpy as np
 import pandas as pd
 from openpyxl.workbook import workbook
 from datetime import date
@@ -27,6 +28,15 @@ try:
     print("Read data from csv file EMP.CSV") #Data frame 
     df_csv = pd.read_csv('Emp.csv') #read Data frme from INput file 
     #print(df_csv)
+    df_csv["Emp Name"] = df_csv["Name Prefix"] + df_csv["First Name"] + df_csv["Middle Initial"] + df_csv["Last Name"]
+    print(df_csv["Emp Name"])
+    df_csv.add(df_csv)
+    #df_csv = df_csv.replace(r'Emp.csv',np.NaN, regex=True)
+    #df_csv.to_csv('modified2.csv')
+    df_csv.drop(columns="Name Prefix",inplace=True)
+    df_csv.drop(columns="First Name",inplace=True)
+    df_csv.drop(columns="Middle Initial",inplace=True)
+    df_csv.drop(columns="Last Name",inplace=True)
     print("convert csv file to comma seperated csv file")
     file_name = "EMPDATA"
     #filename=file_name +"_" + str(today).replace('-', '') + ".csv"
@@ -52,4 +62,5 @@ try:
         print('Done renaming a file')
 except:
     print("An exception occurred");
+print("script is completed sucessfully")
 
