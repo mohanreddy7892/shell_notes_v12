611d3af0c99f03fdbd5712d55c6483ec1070c442
Pasammohanreddy <74665117+mohanreddy7892@users.noreply.github.com>
2023-06-08 09:56:18 +0530

    Update file014.py

--- file014.py
+++ file014.py
@@ -1,6 +1,26 @@
 # some code
-list_data = [2025, 508, 607, 600, 788]
-output = []
-for i in list_data:
-    output.append(''.join(str(i).split("0")))
-print(output)
+import os
+import re
+import pandas as pd
+
+sh_dir = "/path/to/directory"
+data = []
+
+for root, dirs, files in os.walk(sh_dir):
+    for file in files:
+        if file.endswith(".sh"):
+            file_path = os.path.join(root, file)
+            with open(file_path, 'r') as f:
+                content = f.readlines()
+                count_chmod = 0
+                octal_numbers = []
+                for line in content:
+                    matches = re.findall(r'chmod (\d{3})', line)
+                    if matches:
+                        count_chmod += 1
+                        octal_numbers.extend(matches)
+                if octal_numbers:
+                    data.append([file_path, count_chmod, ','.join(octal_numbers)])
+
+df = pd.DataFrame(data, columns=['File Name', 'Count of Chmod', 'Octal Numbers'])
+df.to_excel('chmod_info.xlsx', index=False)
