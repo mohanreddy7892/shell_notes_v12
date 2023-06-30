4ee493e67d5526ef1324adbcf43b9b8e07efa939
Pasammohanreddy <74665117+mohanreddy7892@users.noreply.github.com>
2023-06-30 14:25:07 +0530

    Update FDRD.py

--- FDRD.py
+++ FDRD.py
@@ -1,11 +1,25 @@
-str = input()
-len_str = len(str)
-#print(len_str)
-if len_str != 1:
-    mylist = list(str.strip())
-    first_digit = mylist[0]
-    last_digit = mylist[-1]
-    sum_digits = int(first_digit) + int(last_digit)
-    print(sum_digits)
-else:
-    print(str)
+class CertainCode:
+    @classmethod
+    def returnModifiedSentence(cls, input1):
+        words = input1.split()  # split the string into words
+        result = []
+        for word in words:
+            N = len(word)  # length of the word
+            new_word = ""
+            for ch in word:
+                if ch.isalpha():  # if the character is a letter
+                    ascii_offset = ord('a') if ch.islower() else ord('A')
+                    ascii_end = ord('z') if ch.islower() else ord('Z')
+                    new_ch = chr(ascii_offset + ((ord(ch) - ascii_offset + N) % 26))
+                    if new_ch > chr(ascii_end):
+                        new_ch = chr(ascii_offset + ((ord(new_ch) - ascii_offset - 26) % 26))
+                    new_word += new_ch
+                else:
+                    new_word += ch
+            result.append(new_word)
+        return ' '.join(result)  # join the modified words back into a sentence
+
+# Example usage:
+input1 = "ABCOX"
+output = CertainCode.returnModifiedSentence(input1)
+print(output)  # Output: "FGHIZ"
